60ebfb2a8db862dc1eba929a448ec730f821e0db
Hubert Parzych <hubertparzych10@gmail.com>
2026-05-11 22:01:57 +0200

    enter wreszcie dziala

--- kulki/js/kulki.js
+++ kulki/js/kulki.js
@@ -5,24 +5,32 @@ const n = tab.length;
 
 document.addEventListener("DOMContentLoaded", function () {
     const stage = document.querySelector(".balls-stage");
+    const input = document.querySelector("input.ball");
     for (let i = 0; i < n; i++) {
         const smallBall = document.createElement("p");
         smallBall.classList.add("small-ball");
         smallBall.id = `ball${i + 1}`;
-        smallBall.innerHTML = tab[i];
+        smallBall.textContent = tab[i];
         stage.appendChild(smallBall);
     }
+    function rozszerzKulki() {
+        setTimeout(() => {
+            for (let i = 0; i < n; i++) {
+                const ball = document.getElementById(`ball${i + 1}`);
+                const angle = (i / n) * Math.PI * 2;
+                // cos - x
+                // sin - y
+                // 200 - dalekość
+                // x = r*cos(angle)
+                // y = r*sin(angle)
+                ball.style.transform = `translate(${Math.cos(angle) * 200}px, ${Math.sin(angle) * 200}px)`;
+            }
+        }, 300);
+    }
 
-    setTimeout(() => {
-        for (let i = 0; i < n; i++) {
-            const ball = document.getElementById(`ball${i + 1}`);
-            const angle = (i / n) * Math.PI * 2;
-            // cos - x
-            // sin - y
-            // 200 - dalekość
-            // x = r*cos(angle)
-            // y = r*sin(angle)
-            ball.style.transform = `translate(${Math.cos(angle) * 200}px, ${Math.sin(angle) * 200}px)`;
+    input.addEventListener("keydown", function (p) {
+        if (p.key === "Enter") {
+            rozszerzKulki();
         }
-    }, 300);
+    });
 });
